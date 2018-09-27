ba3e0a2291857b31f7ac6b2f5990fb34f32e85ab
Raman Kooner <ramankooner9@gmail.com>
2018-09-27 15:07:27 -0700

    =2nd Commit

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
+// const path = require('path');
+// const express = require('express');
+//
+// // this goes straight to the public folder without having to go through the server folder
+// const publicPath = path.join(__dirname, '../public');
+// const port = proccess.env.PORT || 3000;
+// // old way of getting public folder
+// // console.log(__dirname + '/../public');
+// //
+// // console.log(publicPath);
+//
+//
+// var app = express();
+//
+// app.use(express.static(publicPath));
+//
+// app.listen(port, () => {
+//   console.log(`Server is up on port ${port}`);
+// });
 const path = require('path');
 const express = require('express');
 
-// this goes straight to the public folder without having to go through the server folder
 const publicPath = path.join(__dirname, '../public');
-const port = proccess.env.PORT || 3000;
-// old way of getting public folder
-// console.log(__dirname + '/../public');
-//
-// console.log(publicPath);
-
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
