2f9099f06a6bd1a17b9d13c9b59afc5f1e1c7705
Raman Kooner <ramankooner9@gmail.com>
2018-09-27 21:35:56 -0700

    Create generateMessage util

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -15,18 +16,11 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+
   // this alerts every user except for the one who joined
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
-  
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
   // we are creating the event
   // sent from server to client
   // socket.emit('newMessage', {
@@ -44,11 +38,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
 
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
 
     // sends event to everyone except this socket
     // socket.broadcast.emit('newMessage', {
