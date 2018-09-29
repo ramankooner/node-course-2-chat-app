a1ac7da0f116748e285356296f73d5a64952a273
Raman Kooner <ramankooner9@gmail.com>
2018-09-29 01:42:10 -0700

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -19,26 +19,41 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-//  console.log('newMessage', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${formattedTime} ${message.from}: ${message.text}`);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+
+  // var li = jQuery('<li></li>');
+  // li.text(`${formattedTime} ${message.from}: ${message.text}`);
+  //
+  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
 
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
+    //var li = jQuery('<li></li>');
     // blank makes us open up the link in a new tab
-    var a = jQuery('<a target = "_blank">My current location</a>')
+    //var a = jQuery('<a target = "_blank">My current location</a>')
 
-    li.text(`${formattedTime} ${message.from}: `);
+    //li.text(`${formattedTime} ${message.from}: `);
     // sets href to the url
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+  //  a.attr('href', message.url);
+  //  li.append(a);
+  //  jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+      from: message.from,
+      url: message.url,
+      createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 // socket.on('newEmail', function (email) {
 //   console.log('New Email', email);
